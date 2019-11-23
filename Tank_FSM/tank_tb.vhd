62e99c8ccd57c59e3777dd5ec5b5c11f71e8e1ba
HannahTHD <36176387+HannahTHD@users.noreply.github.com>
2019-11-22 21:35:19 -0600

    update tank tb

--- Tank_FSM/tank_tb.vhd
+++ Tank_FSM/tank_tb.vhd
@@ -16,13 +16,13 @@ component tank is
 			Speed_up			    							: in std_logic;
 			
 			x					: out std_logic_vector(9 downto 0);
-			y 					: out std_logic_vector(8 downto 0)
+			y 					: out std_logic_vector(9 downto 0)
 		);
 end component tank;
 
 signal rst, en, stop, speed: std_logic;
 signal x_wave : std_logic_vector(9 downto 0);
-signal y_wave : std_logic_vector(8 downto 0);    
+signal y_wave : std_logic_vector(9 downto 0);    
 
 begin
 	dut: tank port map (rst, en, stop, speed, x_wave, y_wave);
@@ -64,9 +64,9 @@ begin
 		speed<='0';
 		wait for 250 ns;
 		speed<='1';
-		wait for 20 ns;
+		wait for 150 ns;
 		speed<='0';
 		wait;
 	end process;
-
+-- wait until? 
 end structural;
