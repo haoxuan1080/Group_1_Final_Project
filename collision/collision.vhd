65b2f852222bc6ce5bec2d1c3a22199ee6ffa3e9
HannahTHD <36176387+HannahTHD@users.noreply.github.com>
2019-11-27 15:09:15 -0600

    change the score and tank

--- collision/collision.vhd
+++ collision/collision.vhd
@@ -5,7 +5,7 @@ use IEEE.numeric_std.all;
 
 entity collision is
 	port(
-			--clock_sys 									: in std_logic;
+			clock	 									: in std_logic;
 			enable										: in std_logic;
 			RESET_sys									: in std_logic;
 			x_tank, y_tank 					      : in std_logic_vector(9 downto 0);
@@ -20,18 +20,22 @@ signal trigger_reg : std_logic;
 
 begin
 
+
+process (RESET_sys,enable, clock) is
+
+begin
+
 trigger <= trigger_reg;
 
-process (RESET_sys,enable) is
-begin
 	if(RESET_sys = '0')then 
 		trigger_reg <= '0';
 
-	elsif((enable = '1') and (((unsigned(x_bullet) - unsigned(x_tank)) < Tank_Width/2)or((unsigned(x_tank) - unsigned(x_bullet)) < Tank_Width/2)) and (((unsigned(y_bullet) - unsigned(y_tank)) < Tank_Hight/2)or((unsigned(y_tank) - unsigned(y_bullet)) < Tank_Hight/2))) then
-			trigger_reg <= '1'; --only one clock?	
-		
-	else 
-		trigger_reg <= '0';
+	elsif(rising_edge(clock))then
+		if((enable = '1')and (((unsigned(x_bullet) - unsigned(x_tank)) < Tank_Width/2) or ((unsigned(x_tank) - unsigned(x_bullet)) < Tank_Width/2)) and (((unsigned(y_bullet) - unsigned(y_tank)) < Tank_Hight/2)or((unsigned(y_tank) - unsigned(y_bullet)) < Tank_Hight/2))) then
+			trigger_reg <= '1'; --only one clock?	s
+		end if;
+--	else 
+--		trigger_reg <= '0';
 		
 	end if;
 	
