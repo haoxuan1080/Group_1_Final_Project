1c8e2a5bfb04c1b73d35d4ae6d53e1af528f0ff1
haoxuan1080 <haoxuanzhang2021@u.northwestern.edu>
2019-12-05 20:53:43 -0600

    keyboard_out_modify_1

--- keyboard/keyboard_out.vhd
+++ keyboard/keyboard_out.vhd
@@ -5,87 +5,210 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity keyboard_out is
 
-port(
-keyboard_clk, keyboard_data, clock_50MHz, reset, enable : in std_logic;--, read : in std_logic;
---ready :out std_logic;
-speed_up1, speed_up2, shoot_out1, shoot_out2 : out std_logic
+	port(
+		--keyboard_clk, keyboard_data, 
+		clock_50MHz, reset: in std_logic;
+		--, read : in std_logic;
+		scan_code: in std_logic_vector (7 downto 0);
+		scan2: in std_logic;
+		--ready :out std_logic;
+		speed_up1_out, speed_up2_out, shoot_1_out, shoot_2_out : out std_logic
 
-);
+	);
 end entity keyboard_out;
 
 
 architecture structural of keyboard_out is
-component ps2 is
-	port( 	keyboard_clk, keyboard_data, clock_50MHz ,
-			reset 		: in std_logic;--, read : in std_logic;
-			
-			scan_code 	: out std_logic_vector( 7 downto 0 );
-			scan_readyo : out std_logic;
-			hist3 : out std_logic_vector(7 downto 0);
-			hist2 : out std_logic_vector(7 downto 0);
-			hist1 : out std_logic_vector(7 downto 0);
-			hist0 : out std_logic_vector(7 downto 0)
-		);
-end component ps2;
+--component ps2 is
+--	port( 	keyboard_clk, keyboard_data, clock_50MHz ,
+--			reset 		: in std_logic;--, read : in std_logic;
+--			
+--			scan_code 	: out std_logic_vector( 7 downto 0 );
+--			scan_readyo : out std_logic;
+--			hist3 : out std_logic_vector(7 downto 0);
+--			hist2 : out std_logic_vector(7 downto 0);
+--			hist1 : out std_logic_vector(7 downto 0);
+--			hist0 : out std_logic_vector(7 downto 0)
+--		);
+--end component ps2;
+signal  delay_code: std_logic_vector(7 downto 0);
+--signal scan2: std_logic;
 
+constant speed1: std_logic_vector (7 downto 0):= x"1A";
+constant shoot1: std_logic_vector (7 downto 0):= x"1C";
+constant speed2: std_logic_vector (7 downto 0):= x"3A";
+constant shoot2: std_logic_vector (7 downto 0):= x"42";
 
-signal ready : std_logic;
+signal speed_up1, speed_up2, shoot_1, shoot_2: std_logic;
+signal sp1_tr, sp2_tr, sh1_tr, sh2_tr: std_logic;
+signal sp1_tr_dly, sp2_tr_dly, sh1_tr_dly, sh2_tr_dly: std_logic;
+
+begin
+
+	speed_up1_out<=speed_up1;
+	speed_up2_out<=speed_up2;
+	shoot_1_out<=shoot_1;
+	shoot_2_out<=shoot_2;
+	--ps2_map: ps2 port map (keyboard_clk=>keyboard_clk, 
+	--		       keyboard_data=>keyboard_data,
+	--		       clock_50MHz=>clock_50MHz,
+	--		       reset=>reset,
+	--		       scan_code=>scan_code,
+	--		       scan_readyo=>scan2,
+	--		       hist3=>open,
+	--		       hist2=>open,
+	--		       hist1=>open,
+	--		       hist0=>open
+	--		      );
+
+	paulse_proc: process (clock_50MHz, reset)
+	begin
+		if (reset='0') then
+			speed_up1<='0';
+			speed_up2<='0';
+			shoot_1<='0';
+			shoot_2<='0';
+			sp1_tr_dly<='0';
+			sp2_tr_dly <='0';
+			sh1_tr_dly<='0';
+			sh2_tr_dly<='0';
+		elsif (rising_edge(clock_50MHz)) then
+			sp1_tr_dly<=sp1_tr;
+			sp2_tr_dly<=sp2_tr;
+			sh1_tr_dly<=sh1_tr;
+			sh2_tr_dly<=sh2_tr;
+			if (sp1_tr_dly='0' and sp1_tr='1') then
+				speed_up1<='1';
+			else
+				speed_up1<='0';
+			end if;
+
+			if (sp2_tr_dly='0' and sp2_tr='1') then
+				speed_up2<='1';
+			else
+				speed_up2<='0';
+			end if;
+
+			if (sh1_tr_dly='0' and sh1_tr='1') then
+				shoot_1<='1';
+			else
+				shoot_1<='0';
+			end if;
+
+			if (sh2_tr_dly='0' and sh2_tr='1') then
+				shoot_2<='1';
+			else
+				shoot_2<='0';
+			end if;
+		end if;
+	end process;
+
+	delay_proc1: process (scan2, reset)
+	begin
+		if (reset='0') then
+			delay_code<=  x"00"; --after 1 ns;
+		elsif(rising_edge(scan2)) then
+			delay_code<=  scan_code; -- after 1 ns;
+		end if;
+	end process;
+
+	tirgger_proc: process (scan2, reset)
+        begin
+                if (reset='0') then
+                        		sp1_tr<='0';
+					sp2_tr<='0';
+					sh1_tr<='0';
+					sh2_tr<='0';
+                elsif(rising_edge(scan2)) then
+                        if (delay_code=x"F0") then
+                               if scan_code=speed1 then
+					sp1_tr<='1';
+					sp2_tr<='0';
+					sh1_tr<='0';
+					sh2_tr<='0';	
+				elsif scan_code=speed2 then
+                                	sp1_tr<='0';
+					sp2_tr<='1';
+					sh1_tr<='0';
+					sh2_tr<='0';
+				elsif scan_code=shoot1 then
+					sp1_tr<='0';
+					sp2_tr<='0';
+					sh1_tr<='1';
+					sh2_tr<='0';
+				elsif scan_code=shoot2 then
+					sp1_tr<='1';
+					sp2_tr<='0';
+					sh1_tr<='0';
+					sh2_tr<='0';
+				end if;
+                        else
+                                sp1_tr<='0';
+				sp2_tr<='0';
+				sh1_tr<='0';
+				sh2_tr<='0';
+                        end if;
+                end if;
+        end process;
+
+end structural;
+--signal ready : std_logic;
 --type state_type is(idle, speed1,speed2, shoot1, shoot2);
 --signal curr_state, next_state: state_type;
-signal trigger, speed_reg1, shoot_reg1, speed_reg2, shoot_reg2, speed_reg1_c, shoot_reg1_c, speed_reg2_c, shoot_reg2_c : std_logic;
-signal curr_code_reg,curr_code, disp_0, disp_1, disp_2, disp_3: std_logic_vector(7 downto 0);
+--signal trigger, speed_reg1, shoot_reg1, speed_reg2, shoot_reg2, speed_reg1_c, shoot_reg1_c, speed_reg2_c, shoot_reg2_c : std_logic;
+--signal curr_code_reg,curr_code, disp_0, disp_1, disp_2, disp_3: std_logic_vector(7 downto 0);
 
-begin 
+--begin 
 
-key_out: ps2 port map(keyboard_clk, keyboard_data, clock_50MHz,reset, curr_code, ready, disp_3, disp_3, disp_1, disp_0);
+--key_out: ps2 port map(keyboard_clk, keyboard_data, clock_50MHz,reset, curr_code, ready, disp_3, disp_3, disp_1, disp_0);
 
-trig_proc: process(clock_50MHz, reset)
-begin
-if(reset = '1')then
+--trig_proc: process(clock_50MHz, reset)
+--begin
+--if(reset = '1')then
 --	trigger <= '0';
-	curr_code_reg <= (others=> '0');
-elsif(rising_edge(clock_50MHz))then
-	curr_code_reg <= curr_code;
+--	curr_code_reg <= (others=> '0');
+--elsif(rising_edge(clock_50MHz))then
+--	curr_code_reg <= curr_code;
 --	if ((disp_1 = X"F0") and (ready = '1')) then
 --		trigger <= '1';		
 --	else
 --		trigger <= '0';
 --	end if;
-end if;
-end process;
+--end if;
+--end process;
 
 
-speed_up1 <= speed_reg1;
-speed_up2 <= speed_reg2;
-shoot_out1 <= shoot_reg1;
-shoot_out2 <= shoot_reg2;
+--speed_up1 <= speed_reg1;
+--speed_up2 <= speed_reg2;
+--shoot_out1 <= shoot_reg1;
+--shoot_out2 <= shoot_reg2;
 
-sequ_proc: process(RESET, clock_50MHz) is
-begin
-		if(RESET = '0') then
-			speed_reg1 <= '0';
-			shoot_reg1 <= '0';
-			speed_reg2 <= '0';
-			shoot_reg2 <= '0';
-		
-		elsif(rising_edge(clock_50MHz))then
-			speed_reg1 <= '0';
-			shoot_reg1 <= '0';
-			speed_reg2 <= '0';
-			shoot_reg2 <= '0';
-			if (disp_1 = X"F0") then
-				if (curr_code_reg = x"1A") then
-					speed_reg1 <= '1';
-				elsif (curr_code_reg = x"4A") then
-					speed_reg2 <= '1';
-				elsif (curr_code_reg = x"1C") then
-					shoot_reg1 <= '1';
-				elsif (curr_code_reg = x"52") then
-					shoot_reg2 <= '1';
-				end if;
-			end if;
-		end if;
-end process;
+--sequ_proc: process(RESET, clock_50MHz) is
+--begin
+--		if(RESET = '0') then
+--			speed_reg1 <= '0';
+--			shoot_reg1 <= '0';
+--			speed_reg2 <= '0';
+--			shoot_reg2 <= '0';
+--		
+--		elsif(rising_edge(clock_50MHz))then
+--			speed_reg1 <= '0';
+--			shoot_reg1 <= '0';
+--			speed_reg2 <= '0';
+--			shoot_reg2 <= '0';
+--			if (disp_1 = X"F0") then
+--				if (curr_code_reg = x"1A") then
+--					speed_reg1 <= '1';
+--				elsif (curr_code_reg = x"4A") then
+--					speed_reg2 <= '1';
+--				elsif (curr_code_reg = x"1C") then
+--					shoot_reg1 <= '1';
+--				elsif (curr_code_reg = x"52") then
+--					shoot_reg2 <= '1';
+--				end if;
+--			end if;
+--		end if;
+--end process;
 	
 --	combinational : process(curr_state, curr_code_reg,trigger) is
 --	begin
@@ -188,4 +311,4 @@ end process;
 --		
 --	end process;
 
-end architecture;
+--end architecture;
---
+++ keyboard/keyboard_out_tb.vhd
@@ -0,0 +1,58 @@
+library ieee;
+
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity keyboard_out_tb is
+
+end keyboard_out_tb;
+
+architecture structural of keyboard_out_tb is
+	component keyboard_out is
+
+	port(
+		--keyboard_clk, keyboard_data, 
+		clock_50MHz, reset : in std_logic;
+		--, read : in std_logic;
+		scan_code: in std_logic_vector (7 downto 0);
+		scan2: in std_logic;
+		--ready :out std_logic;
+		speed_up1_out, speed_up2_out, shoot_1_out, shoot_2_out : out std_logic
+
+	);
+	end component keyboard_out;
+	signal clk, rst_n, scan2, sp1, sp2, sh1, sh2: std_logic;
+	signal scan_code: std_logic_vector (7 downto 0);
+begin
+	dut: keyboard_out port map (clk, rst_n, scan_code, scan2, sp1, sp2, sh1, sh2);
+	clk_proc: process
+	begin
+		clk<='1';
+		wait for 5 ns;
+		clk<='0';
+		wait for 5 ns;
+	end process;
+
+	tb_proc: process
+	begin
+		rst_n<='0';
+		scan_code<=x"00";
+		scan2 <= '0';
+		wait for 6 ns;
+		rst_n<='1';
+		wait for 20 ns;
+		wait until rising_edge(clk);
+		scan_code<=x"F0";
+		scan2<='1';
+		wait for 20 ns;
+		scan2<='0';
+		wait for 26 ns;
+		--wait until rising_edge(clk);
+		scan_code<=x"1C";
+		wait until rising_edge(clk);
+		scan2<='1';
+		wait for 20 ns;
+		scan2<='0';
+		wait;
+	end process;
+end structural;
